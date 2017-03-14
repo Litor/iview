608b09bc21af99f342325c8397104b643e62d8ee
litor <547794529@qq.com>
2017-03-14 16:27:53 +0800

    layout中 content 支持动态组件

--- src/components/container/containerLayout.vue
+++ src/components/container/containerLayout.vue
@@ -1,25 +1,28 @@
 <template>
-  <div>
-    <Row v-for="row in layout" :class="row.class" :style="row.style" v-show="!row.hidden">
-      <i-col :span="col.span" v-for="col in row.cols" :class="col.class" :style="col.style" v-show="!col.hidden">
-        <div v-if="col.content">{{{col.content}}}</div>
-        <div v-if="col.component">
-          <component :is="col.component" :options="col.options || {}" :events="col.events || {}"
-                     class="{{col.name?('comp-name-'+col.name):''}}" :mock="mock"></component>
-        </div>
-        <div v-if="col.layout">
-          <container-layout :layout="col.layout" :mock="mock"></container-layout>
-        </div>
-      </i-col>
-    </Row>
-  </div>
+    <div>
+        <Row v-for="row in rows" :class="row.class" :style="row.style" v-show="!row.hidden">
+            <i-col :span="col.span" v-for="col in row.cols" :class="col.class" :style="col.style" v-show="!col.hidden">
+                <div v-if="col.content">
+                    <content-component  :options="col.options || {}" :events="col.events || {}"
+                               class="{{col.name?('comp-name-'+col.name):''}}" :mock="mock" :content="col.content"></content-component>
+                    </div>
+                <div v-if="col.component">
+                    <component :is="col.component" :options="col.options || {}" :events="col.events || {}"
+                               class="{{col.name?('comp-name-'+col.name):''}}" :mock="mock"></component>
+                </div>
+                <div v-if="col.rows">
+                    <container-layout :layout="col.rows" :mock="mock"></container-layout>
+                </div>
+            </i-col>
+        </Row>
+    </div>
 </template>
 <script>
-  export default {
-      props:{layout:Array, mock:{type:Boolean, default:false}},
-      name:'containerLayout',
-      ready(){
-          console.log(this.layout)
-      }
-  }
+    import contentComponent from './contentComponent.vue'
+
+    export default {
+        components:{contentComponent},
+        props: {rows: Array, mock: {type: Boolean, default: false}},
+        name: 'containerLayout'
+    }
 </script>
