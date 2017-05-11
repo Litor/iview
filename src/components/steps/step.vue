90ee1ec1532e305f33f37428d568e5e58a264b76
litor <547794529@qq.com>
2017-05-11 11:50:17 +0800

    step的title可以设置html

--- src/components/steps/step.vue
+++ src/components/steps/step.vue
@@ -8,7 +8,7 @@
             </div>
         </div>
         <div :class="[prefixCls + '-main']">
-            <div :class="[prefixCls + '-title']">{{ title }}</div>
+            <div :class="[prefixCls + '-title']">{{{ title }}}</div>
             <div v-if="content" :class="[prefixCls + '-content']">{{{ content }}}</div>
         </div>
     </div>
